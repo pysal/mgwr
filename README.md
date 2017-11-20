bc6a76ecb1f5b5137804327b44cb0eb821477814
Taylor Oshan <tayoshan@gmail.com>
2017-11-20 09:11:17 -0700

    initialize gwr submodule

---
+++ README.md
@@ -0,0 +1,20 @@
+**G**eographically **W**eighted **R**egression
+=======================================
+
+Description goes here
+
+Features
+--------
+
+- Feature 1
+- Feature 2
+- Feature 3
+
+Future Work
+-----------
+
+- Future work 1
+- Future work 2
+
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+setup(name='gwr', #name of package
+      version='1.0.0',
+      description='geographically weighted regression', #short <80chr description
+      url='https://github.com/pysal/gwr', #github repo
+      maintainer='Taylor M. Oshan', 
+      maintainer_email='tayoshan@gmail.com', 
+      test_suite = 'nose.collector',
+      tests_require=['nose'],
+      keywords='spatial statistics',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+        ],
+      license='3-Clause BSD',
+      packages=['gwr'],
+      install_requires=['numpy', 'scipy', 'libpysal', 'pysal', 'spglm'],
+      zip_safe=False,
+      cmdclass = {'build.py':build_py})
