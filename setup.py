976d180046c3310de0ebd976c1aade53dbfeacfe
James Gaboardi <jgaboardi@gmail.com>
2018-08-25 21:45:22 -0400

    removing python version below 3.5 in setup.py

--- setup.py
+++ setup.py
@@ -28,10 +28,8 @@ setup(name='mgwr', #name of package
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
         ],
       license='3-Clause BSD',
       packages=['mgwr'],
